318b54eb5ac82e6597bb14b1f7c58f741b81c4bb
yaoqiang <yaoqiangpersonal@gmail.com>
2018-09-26 00:43:22 +0800

    2018-09-26

--- src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.springbootdemo.mapper")
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
